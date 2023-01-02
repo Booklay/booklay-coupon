7581e2fe886a1dc6cf281295036a823bbfb7902e
opgumo <opgumo@naver.com>
2023-01-02 22:01:13 +0900

    initialize repository

---
+++ src/main/java/com/nhnacademy/booklay/booklaycoupon/BooklayCouponApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.booklay.booklaycoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklayCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooklayCouponApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/booklay/booklaycoupon/BooklayCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.booklay.booklaycoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklayCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
