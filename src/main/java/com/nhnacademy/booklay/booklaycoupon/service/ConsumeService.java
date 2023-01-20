a1c24771e5751027f3e52a5f1af9370faff5fd2a
opgumo <opgumo@naver.com>
2023-01-20 09:43:35 +0900

    fix: remove sout

--- src/main/java/com/nhnacademy/booklay/booklaycoupon/service/ConsumeService.java
+++ src/main/java/com/nhnacademy/booklay/booklaycoupon/service/ConsumeService.java
@@ -1,13 +1,15 @@
 package com.nhnacademy.booklay.booklaycoupon.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class ConsumeService {
 
     @KafkaListener(topics = "${message.topic.name}")
     public void consume(String message) {
-        System.out.println(String.format("Consumed message : %s", message));
+        log.info(message);
     }
 }
