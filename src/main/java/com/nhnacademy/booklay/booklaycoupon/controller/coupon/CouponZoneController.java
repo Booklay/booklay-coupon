9548f737536c4955779c0746d84893015457a0f0
opgumo <opgumo@naver.com>
2023-01-30 13:40:28 +0900

    feat: 관리자의 쿠폰존 관리 구현

--- src/main/java/com/nhnacademy/booklay/booklaycoupon/controller/coupon/CouponZoneController.java
+++ src/main/java/com/nhnacademy/booklay/booklaycoupon/controller/coupon/CouponZoneController.java
@@ -1,8 +1,11 @@
 package com.nhnacademy.booklay.booklaycoupon.controller.coupon;
 
 import com.nhnacademy.booklay.booklaycoupon.dto.PageResponse;
+import com.nhnacademy.booklay.booklaycoupon.dto.coupon.request.CouponCURequest;
+import com.nhnacademy.booklay.booklaycoupon.dto.couponzone.request.CouponZoneCreateRequest;
 import com.nhnacademy.booklay.booklaycoupon.dto.couponzone.response.CouponZoneResponse;
 import com.nhnacademy.booklay.booklaycoupon.service.couponzone.CouponZoneService;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +14,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,14 +26,33 @@ public class CouponZoneController {
 
     private final CouponZoneService couponZoneService;
 
-    @GetMapping
-    public ResponseEntity<PageResponse<CouponZoneResponse>> retrieveCouponZone(@PageableDefault
+    @GetMapping("/limited")
+    public ResponseEntity<PageResponse<CouponZoneResponse>> retrieveCouponZoneLimited(@PageableDefault
                                                                                Pageable pageable) {
-        Page<CouponZoneResponse> pages = couponZoneService.retrieveCouponZone(pageable);
+        Page<CouponZoneResponse> pages = couponZoneService.retrieveCouponZoneLimited(pageable);
         PageResponse<CouponZoneResponse> pageResponse = new PageResponse<>(pages);
 
         return ResponseEntity.status(HttpStatus.OK)
             .contentType(MediaType.APPLICATION_JSON)
             .body(pageResponse);
+    }
+
+    @GetMapping("/unlimited")
+    public ResponseEntity<PageResponse<CouponZoneResponse>> retrieveCouponZoneUnlimited(@PageableDefault
+                                                                                      Pageable pageable) {
+        Page<CouponZoneResponse> pages = couponZoneService.retrieveCouponZoneUnlimited(pageable);
+        PageResponse<CouponZoneResponse> pageResponse = new PageResponse<>(pages);
+
+        return ResponseEntity.status(HttpStatus.OK)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(pageResponse);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createCoupon(@Valid @RequestBody
+                                             CouponZoneCreateRequest couponRequest) {
+        couponZoneService.createAtCouponZone(couponRequest);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
--- src/main/java/com/nhnacademy/booklay/booklaycoupon/repository/couponzone/CouponZoneRepository.java
+++ src/main/java/com/nhnacademy/booklay/booklaycoupon/repository/couponzone/CouponZoneRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CouponZoneRepository extends JpaRepository<CouponZone, Long> {
-    Page<CouponZoneResponse> findAllBy(Pageable pageable);
+    Page<CouponZoneResponse> findAllByIsLimitedIs(Boolean isLimited, Pageable pageable);
 }
