82023e8cd64dedb431e5bce3f82df261148b9cb7
오준후 <ojh789ojh@naver.com>
2023-02-02 09:31:33 +0900

    retrieve coupon by productNo memberNo

---
+++ src/main/java/com/nhnacademy/booklay/booklaycoupon/service/coupon/ProductCouponService.java
@@ -0,0 +1,9 @@
+package com.nhnacademy.booklay.booklaycoupon.service.coupon;
+
+import com.nhnacademy.booklay.booklaycoupon.dto.coupon.response.CouponRetrieveResponseFromProduct;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ProductCouponService {
+    Page<CouponRetrieveResponseFromProduct> retrieveCouponPageByMemberNoAndProductNo(Long memberNo, Long productNo, Pageable pageable);
+}
