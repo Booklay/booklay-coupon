c5bfed841eae723d8c965b6d2d0498547535de4e
opgumo <opgumo@naver.com>
2023-02-02 09:26:00 +0900

    doc: todo 추가

--- src/main/java/com/nhnacademy/booklay/booklaycoupon/exception/NotFoundException.java
+++ src/main/java/com/nhnacademy/booklay/booklaycoupon/exception/NotFoundException.java
@@ -1,6 +1,7 @@
 package com.nhnacademy.booklay.booklaycoupon.exception;
 
 public class NotFoundException extends RuntimeException {
+    // Todo 2 : 메시지는 이러함.
     public NotFoundException(String entityName, Long inputId) {
         super("**********Element Not Found**********"
             +"\n Entity Name : " + entityName
