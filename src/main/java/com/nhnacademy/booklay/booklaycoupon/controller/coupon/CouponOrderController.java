13374380b6d69d58ba13520b975ef962ddd80c28
오준후 <ojh789ojh@naver.com>
2023-02-03 15:00:41 +0900

    coupon retrieve by order and productNo

--- src/main/java/com/nhnacademy/booklay/booklaycoupon/controller/coupon/CouponOrderController.java
+++ src/main/java/com/nhnacademy/booklay/booklaycoupon/controller/coupon/CouponOrderController.java
@@ -2,6 +2,7 @@ package com.nhnacademy.booklay.booklaycoupon.controller.coupon;
 
 import com.nhnacademy.booklay.booklaycoupon.dto.PageResponse;
 import com.nhnacademy.booklay.booklaycoupon.dto.coupon.response.CouponRetrieveResponseFromProduct;
+import com.nhnacademy.booklay.booklaycoupon.service.coupon.OrderCouponService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,10 +15,10 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class CouponOrderController {
     private final OrderCouponService orderCouponService;
-    @GetMapping("/{productNo}")
+    @GetMapping
     public ResponseEntity<PageResponse<CouponRetrieveResponseFromProduct>> retrieveAllCoupons(@PageableDefault Pageable pageable
-            , @PathVariable Long productNo, @RequestParam Long memberNo, @RequestParam Boolean isDuplicable) {
-        Page<CouponRetrieveResponseFromProduct> couponPage = orderCouponService.retrieveCouponPageByMemberNoAndProductNo(memberNo, productNo, isDuplicable,  pageable);
+            , @RequestParam Long memberNo, @RequestParam Boolean isDuplicable) {
+        Page<CouponRetrieveResponseFromProduct> couponPage = orderCouponService.retrieveCouponPageByMemberNo(memberNo, isDuplicable, pageable);
         PageResponse<CouponRetrieveResponseFromProduct> couponPageResponse = new PageResponse<>(couponPage);
         return ResponseEntity.ok(couponPageResponse);
     }
