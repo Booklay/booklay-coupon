e9fc9116bd5d0428ed90049a50e42dfaf8773458
opgumo <opgumo@naver.com>
2023-02-03 20:27:03 +0900

    feat: 카프카 연동

--- src/main/java/com/nhnacademy/booklay/booklaycoupon/repository/couponzone/CouponZoneRepository.java
+++ src/main/java/com/nhnacademy/booklay/booklaycoupon/repository/couponzone/CouponZoneRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CouponZoneRepository extends JpaRepository<CouponZone, Long> {
     Page<CouponZoneResponse> findAllByIsLimitedIs(Boolean isLimited, Pageable pageable);
+    boolean existsByCouponId(Long couponId);
 }
