3c873da7753071a45d5d194d2920e2f138725f6f
오준후 <ojh789ojh@naver.com>
2023-02-05 11:36:43 +0900

    memberInfo AOP 추가 ContentCachingRequestWrapperFilter 추가 inputStream 여러번 읽기 가능한 형태로 감싸줌

--- src/main/java/com/nhnacademy/booklay/booklaycoupon/controller/coupon/CouponProductController.java
+++ src/main/java/com/nhnacademy/booklay/booklaycoupon/controller/coupon/CouponProductController.java
@@ -1,6 +1,7 @@
 package com.nhnacademy.booklay.booklaycoupon.controller.coupon;
 
 import com.nhnacademy.booklay.booklaycoupon.dto.PageResponse;
+import com.nhnacademy.booklay.booklaycoupon.dto.common.MemberInfo;
 import com.nhnacademy.booklay.booklaycoupon.dto.coupon.response.CouponRetrieveResponseFromProduct;
 import com.nhnacademy.booklay.booklaycoupon.service.coupon.ProductCouponService;
 import lombok.RequiredArgsConstructor;
@@ -17,8 +18,8 @@ public class CouponProductController {
     private final ProductCouponService productCouponService;
     @GetMapping("/{productNo}")
     public ResponseEntity<PageResponse<CouponRetrieveResponseFromProduct>> retrieveAllCoupons(@PageableDefault Pageable pageable
-            , @PathVariable Long productNo, @RequestParam Long memberNo, @RequestParam Boolean isDuplicable) {
-        Page<CouponRetrieveResponseFromProduct> couponPage = productCouponService.retrieveCouponPageByMemberNoAndProductNo(memberNo, productNo, isDuplicable,  pageable);
+            , @PathVariable Long productNo, MemberInfo memberInfo, @RequestParam Boolean isDuplicable) {
+        Page<CouponRetrieveResponseFromProduct> couponPage = productCouponService.retrieveCouponPageByMemberNoAndProductNo(memberInfo.getMemberNo(), productNo, isDuplicable,  pageable);
         PageResponse<CouponRetrieveResponseFromProduct> couponPageResponse = new PageResponse<>(couponPage);
         return ResponseEntity.ok(couponPageResponse);
     }
