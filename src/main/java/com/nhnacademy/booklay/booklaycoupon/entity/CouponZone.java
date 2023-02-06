c0bf5e39f8a8f7e6b2a77965384e4784d82a0499
opgumo <opgumo@naver.com>
2023-02-06 16:23:49 +0900

    fix: entity 수정

--- src/main/java/com/nhnacademy/booklay/booklaycoupon/entity/CouponZone.java
+++ src/main/java/com/nhnacademy/booklay/booklaycoupon/entity/CouponZone.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import lombok.AccessLevel;
@@ -30,6 +31,10 @@ public class CouponZone {
     @Setter
     @JoinColumn(name = "file_no")
     private ObjectFile file;
+
+    @ManyToOne
+    @JoinColumn(name = "authority_no")
+    private Authority authority;
 
     @Column
     private String name;
