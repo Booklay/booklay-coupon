8958b86575bd1a62ca1108e2b1ad444832be290f
opgumo <opgumo@naver.com>
2023-02-13 09:32:35 +0900

    feat: 사용자의 쿠폰 발급 기록 조회

--- src/main/java/com/nhnacademy/booklay/booklaycoupon/dto/coupon/response/MemberCouponRetrieveResponse.java
+++ src/main/java/com/nhnacademy/booklay/booklaycoupon/dto/coupon/response/MemberCouponRetrieveResponse.java
@@ -1,4 +1,16 @@
 package com.nhnacademy.booklay.booklaycoupon.dto.coupon.response;
 
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class MemberCouponRetrieveResponse {
+    private String name;
+    private int amount;
+    private int minimumUseAmount;
+    private int maximumDiscountAmount;
+    private LocalDateTime expiredAt;
+    private Boolean isDuplicatable;
 }
--- src/main/java/com/nhnacademy/booklay/booklaycoupon/service/coupon/CouponMemberServiceImpl.java
+++ src/main/java/com/nhnacademy/booklay/booklaycoupon/service/coupon/CouponMemberServiceImpl.java
@@ -1,10 +1,16 @@
 package com.nhnacademy.booklay.booklaycoupon.service.coupon;
 
+import com.nhnacademy.booklay.booklaycoupon.dto.coupon.response.CouponHistoryRetrieveResponse;
 import com.nhnacademy.booklay.booklaycoupon.dto.coupon.response.MemberCouponRetrieveResponse;
 import com.nhnacademy.booklay.booklaycoupon.dto.coupon.response.PointCouponRetrieveResponse;
 import com.nhnacademy.booklay.booklaycoupon.repository.coupon.CouponRepository;
+import com.nhnacademy.booklay.booklaycoupon.repository.coupon.OrderCouponRepository;
+import com.nhnacademy.booklay.booklaycoupon.repository.coupon.ProductCouponRepository;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,16 +21,33 @@ import org.springframework.transaction.annotation.Transactional;
 public class CouponMemberServiceImpl implements CouponMemberService{
 
     private final CouponRepository couponRepository;
+    private final OrderCouponRepository orderCouponRepository;
+    private final ProductCouponRepository productCouponRepository;
 
     @Override
     @Transactional(readOnly = true)
     public Page<MemberCouponRetrieveResponse> retrieveCoupons(Long memberNo, Pageable pageable) {
-        return couponRepository.getCouponsByMember(memberNo);
+        List<MemberCouponRetrieveResponse> couponList = new ArrayList<>();
+
+        List<MemberCouponRetrieveResponse> orderCouponList = orderCouponRepository.getCouponsByMember(memberNo);
+        List<MemberCouponRetrieveResponse> productCouponList = productCouponRepository.getCouponsByMember(memberNo);
+
+        couponList.addAll(orderCouponList);
+        couponList.addAll(productCouponList);
+
+        return getPage(pageable, couponList);
     }
 
     @Override
     @Transactional(readOnly = true)
     public Page<PointCouponRetrieveResponse> retrievePointCoupons(Long memberNo, Pageable pageable) {
         return couponRepository.getPointCouponByMember(memberNo, pageable);
+    }
+
+    private Page<MemberCouponRetrieveResponse> getPage(Pageable pageable, List<MemberCouponRetrieveResponse> list) {
+        int start = (int) pageable.getOffset();
+        int end = Math.min((start + pageable.getPageSize()), list.size());
+
+        return new PageImpl<>(list.subList(start, end), pageable, list.size());
     }
 }
