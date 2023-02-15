a46788fe02bd268192776925bfbaa32b024c58b0
opgumo <opgumo@naver.com>
2023-02-15 14:58:40 +0900

    feat: 쿠폰존에 등록된 쿠폰의 발급 가능/불가 시간 조회 추가

--- src/main/java/com/nhnacademy/booklay/booklaycoupon/service/couponzone/CouponZoneService.java
+++ src/main/java/com/nhnacademy/booklay/booklaycoupon/service/couponzone/CouponZoneService.java
@@ -4,6 +4,7 @@ import com.nhnacademy.booklay.booklaycoupon.dto.couponzone.request.CouponZoneCre
 import com.nhnacademy.booklay.booklaycoupon.dto.couponzone.request.CouponZoneIsBlindRequest;
 import com.nhnacademy.booklay.booklaycoupon.dto.couponzone.response.CouponZoneIsBlindResponse;
 import com.nhnacademy.booklay.booklaycoupon.dto.couponzone.response.CouponZoneResponse;
+import com.nhnacademy.booklay.booklaycoupon.dto.couponzone.response.CouponZoneTimeResponse;
 import com.nhnacademy.booklay.booklaycoupon.entity.Coupon;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,13 +13,16 @@ public interface CouponZoneService {
     Page<CouponZoneResponse> retrieveAdminLimited(Pageable pageable);
     Page<CouponZoneResponse> retrieveAdminUnlimited(Pageable pageable);
     Page<CouponZoneResponse> retrieveAdminCouponZoneGraded(Pageable pageable);
+
     Page<CouponZoneResponse> retrieveCouponZoneLimited(Pageable pageable);
     Page<CouponZoneResponse> retrieveCouponZoneUnlimited(Pageable pageable);
     Page<CouponZoneResponse> retrieveCouponZoneGraded(Pageable pageable);
+
     void createAtCouponZone(CouponZoneCreateRequest couponRequest);
     void deleteAtCouponZone(Long couponZoneId);
 
     CouponZoneIsBlindResponse retrieveCouponZoneIsBlind(Long couponZoneId);
-
     void updateIsBlind(Long couponZoneId, CouponZoneIsBlindRequest request);
+
+    CouponZoneTimeResponse retrieveCouponZoneTime(Long couponId);
 }
