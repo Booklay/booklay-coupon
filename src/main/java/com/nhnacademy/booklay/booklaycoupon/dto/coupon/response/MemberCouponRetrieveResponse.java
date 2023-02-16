9d7c631bd6605b2520d57bc689217e570d5face3
opgumo <opgumo@naver.com>
2023-02-17 05:12:03 +0900

    fix: 사용된 쿠폰 수정

--- src/main/java/com/nhnacademy/booklay/booklaycoupon/dto/coupon/response/MemberCouponRetrieveResponse.java
+++ src/main/java/com/nhnacademy/booklay/booklaycoupon/dto/coupon/response/MemberCouponRetrieveResponse.java
@@ -1,6 +1,7 @@
 package com.nhnacademy.booklay.booklaycoupon.dto.coupon.response;
 
 import java.time.LocalDateTime;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -11,7 +12,7 @@ public class MemberCouponRetrieveResponse {
     private final String name;
     private final int amount;
     private final String couponType;
-    private final Long usedItemNo;
+    private final Long itemId;
     private final int minimumUseAmount;
     private final int maximumDiscountAmount;
     private final LocalDateTime expiredAt;
@@ -20,4 +21,37 @@ public class MemberCouponRetrieveResponse {
     private Boolean isUsed = false;
     @Setter
     private String reason = "사용 가능";
+
+    @Builder
+    public MemberCouponRetrieveResponse(String name, int amount, String couponType, Long itemId,
+                                        int minimumUseAmount, int maximumDiscountAmount,
+                                        LocalDateTime expiredAt, Boolean isDuplicatable,
+                                        Boolean isUsed) {
+        this.name = name;
+        this.amount = amount;
+        this.couponType = couponType;
+        this.itemId = itemId;
+        this.minimumUseAmount = minimumUseAmount;
+        this.maximumDiscountAmount = maximumDiscountAmount;
+        this.expiredAt = expiredAt;
+        this.isDuplicatable = isDuplicatable;
+        this.isUsed = isUsed;
+    }
+
+    public static  MemberCouponRetrieveResponse fromOrderCoupon(MemberOrderCouponRetrieveResponse response) {
+        MemberCouponRetrieveResponse retrieveResponse = MemberCouponRetrieveResponse.builder()
+            .name(response.getName())
+            .amount(response.getAmount())
+            .couponType(response.getCouponType())
+            .itemId(response.getItemId())
+            .minimumUseAmount(response.getMinimumUseAmount())
+            .maximumDiscountAmount(response.getMaximumDiscountAmount())
+            .expiredAt(response.getExpiredAt())
+            .isDuplicatable(response.getIsDuplicatable())
+            .isUsed(response.getIsUsed()).build();
+
+        if(retrieveResponse.getIsUsed()) retrieveResponse.setReason("사용 완료");
+
+        return retrieveResponse;
+    }
 }
