75f33dc64c2939d4c986bb1874022ed9594bc4d6
opgumo <opgumo@naver.com>
2023-02-21 17:10:47 +0900

    doc: rest doc 추가 - couponAdminController

--- src/test/java/com/nhnacademy/booklay/booklaycoupon/controller/coupon/CouponMemberControllerTest.java
+++ src/test/java/com/nhnacademy/booklay/booklaycoupon/controller/coupon/CouponMemberControllerTest.java
@@ -2,28 +2,42 @@ package com.nhnacademy.booklay.booklaycoupon.controller.coupon;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nhnacademy.booklay.booklaycoupon.dto.coupon.response.MemberCouponRetrieveResponse;
 import com.nhnacademy.booklay.booklaycoupon.dto.coupon.response.PointCouponRetrieveResponse;
 import com.nhnacademy.booklay.booklaycoupon.service.coupon.CouponMemberService;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "docs.api.com")
+@ExtendWith(RestDocumentationExtension.class)
 @WebMvcTest(CouponMemberController.class)
 @MockBean(JpaMetamodelMappingContext.class)
 class CouponMemberControllerTest {
@@ -37,9 +51,9 @@ class CouponMemberControllerTest {
     @Autowired
     MockMvc mockMvc;
 
-    private static final String URI_PREFIX = "/members/1/coupons";
-
-    ObjectMapper objectMapper = new ObjectMapper();
+    String DOC_PREFIX = "members/{memberNo}/coupons";
+    String URI_PREFIX = "/" + DOC_PREFIX;
+    Long targetId = 1L;
 
     @Test
     @DisplayName("회원이 소유한 쿠폰 조회 성공.")
@@ -49,13 +63,23 @@ class CouponMemberControllerTest {
         PageImpl<MemberCouponRetrieveResponse> response = new PageImpl<>(List.of(), pageRequest, 1);
 
         // when
-        when(couponMemberService.retrieveCoupons(1L, pageRequest)).thenReturn(response);
+        when(couponMemberService.retrieveCoupons(targetId, pageRequest)).thenReturn(response);
 
         // then
-        mockMvc.perform(get(URI_PREFIX)
+        mockMvc.perform(RestDocumentationRequestBuilders.get(URI_PREFIX, targetId)
                 .accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andDo(print())
+            .andDo(document(DOC_PREFIX + "/{methodName}",
+                preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()),
+                responseFields(
+                    fieldWithPath("pageNumber").description("현재 페이지"),
+                    fieldWithPath("pageSize").type(JsonFieldType.NUMBER).description("페이지 크기"),
+                    fieldWithPath("totalPages").description("총 페이지"),
+                    fieldWithPath("data").description("현재 페이지의 데이터")),
+                pathParameters(
+                    parameterWithName("memberNo").description("회원의 No"))
+            ))
             .andReturn();
 
         Mockito.verify(couponMemberService).retrieveCoupons(any(), any());
